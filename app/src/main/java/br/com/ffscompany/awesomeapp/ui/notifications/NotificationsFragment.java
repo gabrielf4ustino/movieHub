3cd9e4eefebdbda427e56eb528b598f677ad17e3
João Pedro M <joaopedromeloroberto@gmail.com>
2023-04-15 21:14:16 -0400

    Spinner & Recycler

--- app/src/main/java/br/com/ffscompany/awesomeapp/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/br/com/ffscompany/awesomeapp/ui/notifications/NotificationsFragment.java
@@ -2,17 +2,23 @@ package br.com.ffscompany.awesomeapp.ui.notifications;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.navigation.NavigationBarView;
+
+import br.com.ffscompany.awesomeapp.R;
 import br.com.ffscompany.awesomeapp.databinding.FragmentNotificationsBinding;
 
-public class NotificationsFragment extends Fragment {
+public class NotificationsFragment extends Fragment  {
 
     private FragmentNotificationsBinding binding;
 
@@ -22,8 +28,13 @@ public class NotificationsFragment extends Fragment {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        Spinner spinner = binding.genSpinner;
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.generos,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
         return root;
     }
 
